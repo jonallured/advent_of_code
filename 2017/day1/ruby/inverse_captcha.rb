86146098873dbeba0aa37c9bc9c447e1c91b4198
Jon Allured <jon.allured@gmail.com>
2017-12-01 10:09:12 -0600

    Remove stale guard
    This was how I was guarding against wrapping in the first context, but
    now that I have a more robust guard, I can remove this.

--- 2017/day1/ruby/inverse_captcha.rb
+++ 2017/day1/ruby/inverse_captcha.rb
@@ -18,7 +18,7 @@ class InverseCaptcha
     @numbers.each_with_index.map do |n, index|
       next_index = index + @steps
       next_index = next_index - @numbers.length if next_index >= @numbers.length
-      next_item = next_index == @numbers.length ? @numbers.first : @numbers[next_index]
+      next_item = @numbers[next_index]
       n == next_item ? n : 0
     end
   end
